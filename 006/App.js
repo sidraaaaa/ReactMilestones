2bad933fd852b5b8d7235f2e547c93a8fc8ee8dc
sidraaaaa <31964272+sidraaaaa@users.noreply.github.com>
2019-05-19 08:58:34 +0500

    Create App.js

---
+++ 006/App.js
@@ -0,0 +1,47 @@
+import React,{Component} from 'react';
+import './App.css';
+import Header from './Component/layout/header';
+import Todos from './Component/Todos';
+import AddTodo from './Component/AddTodo'
+
+class App extends Component {
+  state={
+    todos:[
+      {id:1, title:'Wake up early', completed:true},
+      {id:2, title:'Perform ablution', completed:true},
+      {id:3, title:'Pray fajr', completed:false}
+    ]
+
+  }
+  //Toggle Complete
+  markComplete=(id)=>{
+    this.setState({todos: this.state.todos.map(todo=>{
+      if(todo.id===id){
+        todo.completed=!todo.completed
+      }
+      return todo;
+    })});
+  }
+
+  //Delete
+  delTodo=(id)=>{
+    this.setState({todos:[...this.state.todos.filter(todo=>todo.id
+      !==id)]});
+  }
+
+  render() {
+    return(
+      <div className='App'>
+        <div className="container">
+        <Header/>
+        <AddTodo/>
+        <Todos todos= {this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+       </div>
+
+        </div>
+    );}
+}
+
+
+
+export default App;
