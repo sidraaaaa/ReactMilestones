c7bfa1ff7a8d061184aa27d8dd5a0b31778513e0
sidra <sidra_rehmani1@hotmail.com>
2019-05-21 14:15:59 -0400

    Create header.js

---
+++ 009/Component/layout/header.js
@@ -0,0 +1,27 @@
+//function based component
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+export default function header() {
+  return (
+    <header style={headerStyle}>
+        <h1>TodoList</h1>
+        <Link style={linkStyle} to="/">Home</Link> | 
+        <Link style={linkStyle} to="/about">About</Link>
+        </header>
+  )
+}
+
+//we cant use a tag directly so importing is mandatory
+
+const headerStyle={
+    background:'#333',
+    color:'#fff',
+    textAlign: 'center',
+    padding:'10px'
+}
+
+const linkStyle={
+    color:'#fff',
+    textDecoration:'none'
+}
