1133f73ba54f849cba103897e1746661303e2530
sidra <sidra_rehmani1@hotmail.com>
2019-05-28 15:48:37 -0400

    lists

---
+++ 016/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Component} from 'react';
+import Ninja from './Ninja';
+
+class App extends Component {
+state={
+  ninjas:[
+  {name:"Fred", age:"15" ,task:"done", id:1},
+  {name:"Greg", age:"18" ,task:"not done", id:2},
+  {name:"George", age:"20", task:"done", id:3}
+]
+
+}
+
+
+  render(){
+  return (
+    <div className="App">
+
+     <h1>Check it off</h1>
+     <h2>React with react js</h2>
+     <Ninja ninjas={this.state.ninjas}/> 
+    </div>
+  );
+}
+//The properties passed in Ninja's component, will be imparsed in Ninja.js through class based Component 
+}
+
+export default App;
