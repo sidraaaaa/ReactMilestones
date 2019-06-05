37c9fec5bb1d868541a41aa96202365c968827c8
sidra <sidra_rehmani1@hotmail.com>
2019-06-04 23:14:24 -0400

    deleting through filter

---
+++ 023/AddNinja.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+export class AddNinja extends Component {
+    state={
+        name:null,
+        age:null,
+        task:null 
+    }
+    handleChange=(e)=>{
+        this.setState({
+            [e.target.id]:e.target.value
+        })
+    }
+    handleSubmit=(e)=>{
+        e.preventDefault();
+        this.props.addNinja(this.state
+        );
+    }
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                 
+                <label htmlFor="name">Name: </label>
+                 <input type="text" id="name" onChange={this.handleChange}/>
+                 <label htmlFor="name">Age: </label>
+                 <input type="text" id="age" onChange={this.handleChange}/>
+                 <label htmlFor="name">Task: </label>
+                 <input type="text" id="task" onChange={this.handleChange}/>
+                 <button>Submit</button>   
+                    </form>
+            </div>
+        )
+    }
+}
+
+export default AddNinja
---
+++ 023/Ninja.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+//building functional component:
+//when we dont need states and are concerned with UI
+//destructuring directly as we receive the props
+//const{ninjas}=props; into const Ninjas=({ninjas})
+const Ninjas=({ninjas,deleteNinja})=>{
+    //props help to transfer data fom parent to child
+   //this is a js func we can add js in it
+       // console.log(this.props);
+       //we can get props by destructuring
+       //we dont need to call the properties individually by this.props.name in return
+      // const {name,age}=this.props;
+      
+     // const ninjaList=ninjas.map(ninja=>
+       // {
+            /*
+            return (
+             <div className="ninja" key={ninja.id}>
+                <div>name:{ninja.name}</div>
+                <div>age:{ninja.age}</div>
+                <div>task:{ninja.task}</div>
+            
+            </div>
+                   )
+        }                       )
+       */
+       //<button onClick={deleteNinja(ninja.id)}>x</button> automatically invoking func by using()
+        
+        return (
+            <div className="ninja-list">
+                {
+                 ninjas.map(ninja=>{
+           return ninja.age<20?(
+           <div className="ninja" key={ninja.id}>
+           <div>name:{ninja.name}</div>
+           <div>age:{ninja.age}</div>
+           <div>task:{ninja.task}</div>
+           <button onClick={()=>{deleteNinja(ninja.id)}}>x</button>
+           
+       
+       </div>
+       
+       ):null;
+        })
+                }
+            </div>
+             
+          )
+        }
+
+export default Ninjas
