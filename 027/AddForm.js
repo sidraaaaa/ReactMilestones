2cd89f9fe5623659b17adf1bc5ad21f62be2ae23
sidra <sidra_rehmani1@hotmail.com>
2019-06-09 00:26:10 -0400

    Todo part2

---
+++ 027/AddForm.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+export class AddTodo extends Component {
+    state={
+        content:''
+    }
+
+    handleChange=(e)=>{
+        this.setState({
+            content:e.target.value
+        })
+    }
+    handleSubmit=(e)=>{
+        e.preventDefault();
+        console.log(this.state);
+    }
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Add new todo: </label>
+                    <input type="text" onChange={this.handleChange}/>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddTodo
