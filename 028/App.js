b7d527c0e7fc606db4a9e38b8416984e30f608bd
sidra <sidra_rehmani1@hotmail.com>
2019-06-09 19:52:31 -0400

    Todo part2

---
+++ 028/App.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import Todos from './Todos'
+import AddTodo from './AddForm'
+
+class App extends Component {
+  state={
+    todos:[
+      {id:1,content:"buy some milk"},
+      {id:2, content:"play mario"}
+    ]
+  }
+
+  deleteTodo=(id)=>{
+   const todos=this.state.todos.filter(
+     todo=>{
+       return todo.id!==id
+     }
+   );
+
+   this.setState({
+     todos:todos
+   })
+  }
+
+  addTodo=(todo)=>{
+    todo.id=Math.random();
+    let todos=[...this.state.todos,todo];
+    this.setState({
+      todos
+    })
+  }
+  render (){
+    return(
+    <div className="todo-pp container">
+     <h1 className="center orange-text">Todos List</h1>
+     <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+      <AddTodo addTodo={this.addTodo}/>
+    </div>
+  );
+}
+}
+
+export default App;
