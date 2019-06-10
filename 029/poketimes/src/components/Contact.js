eab67d3292fb3bb8c26adc7d68f18512627ab564
sidra <sidra_rehmani1@hotmail.com>
2019-06-10 19:01:43 -0400

    Add files via upload

---
+++ 029/poketimes/src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+const Contact=()=>{
+    return(
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Natus excepturi enim sunt hic maiores aliquid ab est unde optio dolores. 
+                Dolorem quos obcaecati officia placeat cumque saepe provident voluptatibus animi?
+                </p>
+        </div>
+    )
+}
+export default Contact
---
+++ 029/poketimes/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+const Home=()=>{
+    return(
+        <div>
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Natus excepturi enim sunt hic maiores aliquid ab est unde optio dolores. 
+                Dolorem quos obcaecati officia placeat cumque saepe provident voluptatibus animi?
+                </p>
+        </div>
+        </div>
+    )
+}
+export default Home
