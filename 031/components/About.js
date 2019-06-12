4355c8e0b1c662a0abb494dccc680b5b779eef74
sidra <sidra_rehmani1@hotmail.com>
2019-06-12 11:50:13 -0400

    routing through components using single page

---
+++ 031/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+const About=()=>{
+    return(
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Natus excepturi enim sunt hic maiores aliquid ab est unde optio dolores. 
+                Dolorem quos obcaecati officia placeat cumque saepe provident voluptatibus animi?
+                </p>
+        </div>
+    )
+}
+export default About
