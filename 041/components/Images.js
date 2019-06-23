111d4e3b8ff65d58859019d7d8795df0a334950d
sidra <sidra_rehmani1@hotmail.com>
2019-06-22 22:48:10 -0400

    Create Images.js

---
+++ 041/components/Images.js
@@ -0,0 +1,11 @@
+import React from 'react'
+const Images=(props)=>{
+    return(
+        <div className="container">
+            <h4 className="center">Images</h4>
+           
+                
+        </div>
+    )
+}
+export default Images
