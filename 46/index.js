d533dfe25f303cbc9ee828bd714fe5e89fe6f4d0
sidra <sidra_rehmani1@hotmail.com>
2019-06-27 22:49:17 -0400

    react+redux

---
+++ 46/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux'; // the Provider is a component that will surround App component.
+
+const store= createStore();
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+
+serviceWorker.unregister();
