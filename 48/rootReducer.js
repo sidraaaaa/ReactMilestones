0ac232a3b45817a67d7595995535678394f7abc4
sidra <sidra_rehmani1@hotmail.com>
2019-06-29 22:26:30 -0400

    connecting redux store with react and applying state to props

---
+++ 48/rootReducer.js
@@ -0,0 +1,10 @@
+const initState={
+    posts:[
+    {id:'1',title:'Squirtle laid an egg', body:'lorem ipsim'},
+    {id:'2',title:'this is a title',body:'this is its body'},
+    ]
+}
+const rootReducer=(state=initState,action)=>{
+return state;
+}
+export default rootReducer 
