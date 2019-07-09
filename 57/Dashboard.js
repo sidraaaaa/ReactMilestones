b6adcec9e581d569e90eed53cb59a00b56968f0e
sidra <sidra_rehmani1@hotmail.com>
2019-07-08 22:14:39 -0400

    Create Dashboard.js

---
+++ 57/Dashboard.js
@@ -0,0 +1,20 @@
+import React,{component} from 'react';
+
+export class Dashboard extends Component {
+    render() {
+        return (
+            <div className="dashboard container">
+                <div className="row">
+                    <div className="col s12 m6"> </div>
+                    <div className="col s12 m5 offset-m1"></div>
+
+                        
+                
+                </div>
+                
+            </div>
+        )
+    }
+}
+
+export default Dashboard
