c24677d8530afd35a58b1a6b3b98e953c550f680
sidra <sidra_rehmani1@hotmail.com>
2019-07-10 22:28:14 -0400

    Add files via upload

---
+++ 59/ProjectList.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+
+const ProjectList=()=>{
+    return(
+        <div className="project-list section">
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by xyz</p>
+                    <p className="grey-text">July 11th,2019</p>
+                </div>
+            </div>
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by xyz</p>
+                    <p className="grey-text">July 11th,2019</p>
+                </div>
+            </div>
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by xyz</p>
+                    <p className="grey-text">July 11th,2019</p>
+                </div>
+            </div>
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by xyz</p>
+                    <p className="grey-text">July 11th,2019</p>
+                </div>
+            </div>
+        </div>
+        
+
+    )
+}    
+
+
+export default ProjectList
