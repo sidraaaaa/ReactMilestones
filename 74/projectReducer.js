943b5c2c2a0e31e4b64863ab7ad1d6ba351e0826
sidra <sidra_rehmani1@hotmail.com>
2019-07-25 20:12:01 -0400

    corrected file

---
+++ 74/projectReducer.js
@@ -0,0 +1,13 @@
+const initState = {
+    projects:[
+    {id:'1',title:'help me find peach', content:'some extra lil offer'},
+    {id:'2',title:'join admissions team', conetent:'project workout'},
+    {id:'3',title:'focus on scholarship details',content:'check out your whole summary'}
+]
+}
+
+const projectReducer = (state = initState, action) => {
+  return state;
+};
+
+export default projectReducer;
