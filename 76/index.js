5173d4b4ab3c8e860d74b32ae2c73480470073fe
sidra <sidra_rehmani1@hotmail.com>
2019-07-27 14:52:37 -0400

    Create index.js

---
+++ 76/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+import rootReducer from './store/reducers/rootReducer';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+//applyMiidleWare func takes in a list of middleware
+// and it turns into a store enhancer
+// and we could add many diff middlewares in it
+//thunk returns a func inside our action creators which can then interact with a db
+
+const store=createStore(rootReducer,applyMiddleware(thunk));
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
