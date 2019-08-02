6eb8bb308be59874482bece056b53d5a1df69f02
sidra <sidra_rehmani1@hotmail.com>
2019-08-02 00:26:43 -0400

    The component on which our projects are listed on home page

---
+++ 82/projectReducer.js
@@ -0,0 +1,25 @@
+const initState = {
+    projects:[
+    {id:'1',title:'help me find peach', content:'some extra lil offer'},
+    {id:'2',title:'join admissions team', conetent:'project workout'},
+    {id:'3',title:'focus on scholarship details',content:'check out your whole summary'}
+]
+}
+//projectReducer received the project which is dispatched from createProject.js and projectActions.js
+
+
+const projectReducer = (state = initState, action) => {
+  //first checking the action type
+  switch(action.type){
+    case 'CREATE_PROJECT':
+      console.log('created project',action.project)
+      return state;
+    case 'CREATE_PROJECT_ERROR':
+      console.log('create project error',action.error);
+      return state;
+    default:
+      return state;
+    }
+};
+
+export default projectReducer;
