d40996309b12f3ec7e3693aec6504cc3d4b4c76c
sidra <sidra_rehmani1@hotmail.com>
2019-08-18 01:00:42 -0400

    On 98th day, redirecting the links

---
+++ 98/Index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {createStore,applyMiddleware,compose} from 'redux';
+import rootReducer from './store/reducers/rootReducer';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+//applyMiidleWare func takes in a list of middleware
+// and it turns into a store enhancer
+// and we could add many diff middlewares in it
+//thunk returns a func inside our action creators which can then interact with a db
+import {reduxFirestore, getFirestore} from 'redux-firestore';
+import {reactReduxFirebase, getFirebase} from 'react-redux-firebase';
+import fbConfig from './config/fbConfig';
+
+
+const store = createStore(rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+    reactReduxFirebase(fbConfig, {attachAuthIsReady: true}), // redux binding for firebase
+    reduxFirestore(fbConfig) // redux bindings for firestore
+  )
+);
+//withExtraArgument allows to pass more args in projectAction's argument
+store.firebaseAuthIsReady.then(() => {
+  ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root')); 
+  serviceWorker.unregister();
+});
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+
+
